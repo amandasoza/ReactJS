a63d64302359cf9e4ed72c609393a365c3c0c59b
Amanda <donginyo@gmail.com>
2024-11-29 22:50:31 -0300

    Estilos com React

--- typescriptapp/src/App.tsx
+++ typescriptapp/src/App.tsx
@@ -1,25 +1,11 @@
-import React from 'react';
-import './App.css';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'; 
-import Login from './pages/login.page';  
-import firebaseConfig from './firebaseConfig';
-import { initializeApp } from 'firebase/app';
-import Home from './pages/home.page';  
-
-
-export const app = initializeApp(firebaseConfig);
+import './App.scoped.css';  
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} /> 
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <div className="container">
+      <h1 className="title">Hello, World!</h1>
+    </div>
   );
 }
 
 export default App;
-
-
